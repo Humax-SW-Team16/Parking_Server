76d5b53b7d2bbcf2211b519c163f6f3449f152a3
CYJhub <yejee1018@naver.com>
2023-12-07 15:21:50 +0900

    redis 구현 중

--- src/main/java/com/humax/parking/service/UserService.java
+++ src/main/java/com/humax/parking/service/UserService.java
@@ -4,19 +4,55 @@ import com.humax.parking.dto.*;
 import com.humax.parking.model.ParkingEntity;
 import com.humax.parking.repository.UserRepository;
 import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
 @Transactional
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserService {
 
+    private final RedisTemplate<String, Integer> redisTemplate;
+
+    private final HashOperations<String, String, Integer> hashOperations;
+
+    private static final String SEARCH_COUNT_KEY = "parking_search_count";
+
     private final UserRepository userRepository;
+
+//    private void updateSearchCount(List<ParkingEntity> parkingEntities){
+//        for(ParkingEntity parkingEntity : parkingEntities){
+//            parkingEntity.setSearchCount(parkingEntity.getSearchCount()+1);
+//        }
+//    }
+    public void increaseParkingSearchCount(String parkingId) {
+      String parkingKey = SEARCH_COUNT_KEY + ":" + parkingId;
+      Long count = redisTemplate.opsForValue().increment(parkingKey, 1);
+      if (count == null || count == 1) {
+          // Expire after 24 hours (86400 seconds)
+          redisTemplate.expire(parkingKey, 86400, TimeUnit.SECONDS);
+    }
+}
+
+    // Redis에서 특정 주차장 ID(parkingId)에 대한 검색 횟수를 가져오는 메서드
+    public Long getParkingSearchCount(String parkingId) {
+        //주차장 검색 횟수를 저장하기 위한 Redis 키의 기본 값
+        //이 키는 주차장 ID와 함께 조합하여 특정 주차장에 대한 검색 횟수를 저장하는 데 사용
+        String parkingKey = SEARCH_COUNT_KEY + ":" + parkingId;
+        Object count = redisTemplate.opsForValue().get(parkingKey);//parkingKey=특정 주차장의 검색 횟수를 저장하는데 사용
+        return count != null ? (Long) count : 0L;
+    }
+
 
     public List<ParkingInfoDTO> findNearbyParking(UserLocationDTO userLocationDTO){
         try{
@@ -60,9 +96,4 @@ public class UserService {
     }
 
 
-    private void updateSearchCount(List<ParkingEntity> parkingEntities){
-        for(ParkingEntity parkingEntity : parkingEntities){
-            parkingEntity.setSearchCount(parkingEntity.getSearchCount()+1);
-        }
-    }
 }
